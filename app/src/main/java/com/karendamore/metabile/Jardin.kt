764ce6e3e0331cec40bc9103282e395a29a38d28
KarenDamore <kdramirezm@gmail.com>
2021-11-20 10:13:49 -0500

    Scrollview, interfaz card_view, interfaz list

---
+++ app/src/main/java/com/karendamore/metabile/Jardin.kt
@@ -0,0 +1,4 @@
+package com.karendamore.metabile
+
+data class Jardin (
+    val nombre: String, val descripcion: String, val puntuacion: String)
---
+++ app/src/main/java/com/karendamore/metabile/ListJardinActivity.kt
@@ -0,0 +1,11 @@
+package com.karendamore.metabile
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ListJardinActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_list_jardin)
+    }
+}
